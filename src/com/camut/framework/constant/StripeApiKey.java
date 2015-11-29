3fd7c930ad14018a58aff1b448c364c41c258d6c
JinTao1126 <jim_taojin@hotmail.com>
2015-11-29 15:45:10 -0700

    update stripe Key

--- src/com/camut/framework/constant/StripeApiKey.java
+++ src/com/camut/framework/constant/StripeApiKey.java
@@ -14,10 +14,10 @@ package com.camut.framework.constant;
  */
 public class StripeApiKey {
 	
-	public final static String TESTPUBLISHKEY="pk_test_iwMqezkfoq23l6fiDxdvhg1A";//测试publishkey
-	public final static String TESTAPIKEY="sk_test_D9BCxpj4Naw8GdlZmPDHKvlL";//测试apikey
+	public final static String TESTPUBLISHKEY="pk_test_iwMqezkfoq23l6fiDxdvhg1A";//TEST publishkey
+	public final static String TESTAPIKEY="sk_test_D9BCxpj4Naw8GdlZmPDHKvlL";//TEST apikey
 	
-	public final static String LIVEPUBLISHKEY="pk_live_xovT9dFAtWbDUmouH39ULcMq";//正式publishkey
-	public final static String LIVEAPIKEY="sk_live_yZEoNLRlxtaW3Vh4cX21jthp";//测试apikey
+	public final static String LIVEPUBLISHKEY="pk_live_5NwDKWZDQsHAYkg2l8la8bLP";//PROD publishkey
+	public final static String LIVEAPIKEY="sk_live_1dwiAHiXRpjbuHhkBs65syXL";//PROD apikey
 	
 }
