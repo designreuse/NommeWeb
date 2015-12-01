7b6ec36e641e70e665c5732f109cf343e51a2dde
Cuong Ich Truong <cuongichtruong@gmail.com>
2015-12-01 16:40:59 -0700

    Update utli and global constants
    ~DateUlti.java: add functions: addHours, ToDate,
    ~DateUtilTest.java: add unit tests for addHours(), ToDate()
    ~GlobalConstant.java: add enum for order status, order type and
    restaurant status

--- src/com/camut/utils/DateUtil.java
+++ src/com/camut/utils/DateUtil.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.joda.time.DateTime;
+
 
 public class DateUtil {
 	
@@ -22,6 +24,14 @@ public class DateUtil {
 		return cal.getTime();
 	}
 	
+	public static Date AddHours(Date date, int offset)
+	{
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.HOUR, offset);
+		return cal.getTime();
+	}
+	
 	public static Date SetToMidnightTime(Date date)
 	{
 		if(date != null){
@@ -32,10 +42,24 @@ public class DateUtil {
 				Log4jUtil.info("Something went wrong when trying to convert DateTime to Date in function addOrder()");
 			}
 		}
-		Log4jUtil.info("SetToMidnightTime(Date date) set time to midnight failed");
+		Log4jUtil.info("SetToMidnightTime(Date date): input value is NULL");
 		
 		return null;
 	}
 	
+	public static Date ToDate(DateTime dateTime)
+	{
+		if(dateTime != null){
+			try {
+				return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+						.parse(dateTime.toString("yyyy-MM-dd HH:mm:ss"));
+			} catch (ParseException e) {
+				Log4jUtil.error(e);
+				Log4jUtil.info("Something went wrong when trying to convert DateTime to Date");
+			}
+		}
+		Log4jUtil.info("ToDate(DateTime dateTime) failed: input value is NULL");
+		return null;
+	}
 
 }
